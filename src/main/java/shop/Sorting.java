12394571234d5d4d2276dd3b14cd7c115d4b89bf
Octaviany <63240238+octav17@users.noreply.github.com>
2020-06-22 01:10:41 +0700

    Update Sorting.java

--- src/main/java/shop/Sorting.java
+++ src/main/java/shop/Sorting.java
@@ -23,3 +23,25 @@ public class Sorting {
         inventory.sort(new Comparator<Apple>() {
             public int compare(Apple a1, Apple a2){
                 return a1.getWeight().compareTo(a2.getWeight()); 
+        }});
+        System.out.println(inventory);
+        
+        
+        
+        
+        public static class Apple {
+        private Integer weight = 0;
+        private String color = "";
+
+        public Apple(Integer weight, String color){
+            this.weight = weight;
+            this.color = color;
+        }
+
+        public Integer getWeight() {
+            return weight;
+        }
+
+        public void setWeight(Integer weight) {
+            this.weight = weight;
+        }
