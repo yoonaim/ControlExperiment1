b7d16b584006cc3eab7a06e225211163dfefcb51
Octaviany <63240238+octav17@users.noreply.github.com>
2020-06-22 01:21:31 +0700

    Update Finding.java

--- src/main/java/shop/Finding.java
+++ src/main/java/shop/Finding.java
@@ -23,4 +23,17 @@ public class Finding{
     private static boolean isVegetarianFriendlyMenu(){
         return menu.stream().anyMatch(Dish::isVegetarian);
     }
+    
+    private static boolean isHealthyMenu(){
+        return menu.stream().allMatch(d -> d.getCalories() < 1000);
+    }
+    
+    private static boolean isHealthyMenu2(){
+        return menu.stream().noneMatch(d -> d.getCalories() >= 1000);
+    }
+    
+    private static Optional<Dish> findVegetarianDish(){
+        return menu.stream().filter(Dish::isVegetarian).findAny();
+    }
+    
 }
