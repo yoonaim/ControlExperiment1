2d8b7fc49459d28847aebe7d7803b5ebcd91b60e
Ventina Limbong <ventinabong@gmail.com>
2020-06-21 14:48:15 +0700

    adding ordersController

---
+++ ordersController.java
@@ -0,0 +1,40 @@
+package com.example.ppw.controller;
+
+import com.example.ppw.model.orders;
+import com.example.ppw.repository.ordersRepo;
+import com.example.ppw.repository.testNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/shoppingcart")
+public class ContohKondisi {
+
+    public static void main(String[] args) {
+        boolean kondisi;
+
+        kondisi= (1+1) == 2 && 2>1;
+
+        if (kondisi){
+            System.out.println("Jawaban anda benar...");
+        }
+        else {
+            System.out.println("Jawaban anda benar salah...");
+        }
+    }
+}
+
+class CetakAngka{
+/*contoh membuat variabel dan scanner
+ contoh komentar*/
+    public static void main(String[] argumen){
+
+        for(int i=0; i <= 10; i++){
+            System.out.print( i + " " );
+        }
+
+    }
+}
