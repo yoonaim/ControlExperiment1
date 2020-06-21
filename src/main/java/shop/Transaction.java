ac6bf01a70d75a0800bd87ee59118b8ec837d747
Octaviany <63240238+octav17@users.noreply.github.com>
2020-06-22 01:22:47 +0700

    Update Transaction.java

--- src/main/java/shop/Transaction.java
+++ src/main/java/shop/Transaction.java
@@ -1,6 +1,16 @@
 package lambdasinaction.chap5;
 
 public class Transaction{
+  
+  private Trader trader;
+	private int year;
+	private int value;
 
+	public Transaction(Trader trader, int year, int value)
+	{
+		this.trader = trader;
+		this.year = year;
+		this.value = value;
+	}
 
 }
