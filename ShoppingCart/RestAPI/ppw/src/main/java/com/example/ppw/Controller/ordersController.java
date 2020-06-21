fb8311fa11b1b57acb4476df0980511c6e2deffe
Markolim <54051867+Markolim@users.noreply.github.com>
2020-06-21 23:39:48 +0700

    update class AddTwoNumbers

--- ShoppingCart/RestAPI/ppw/src/main/java/com/example/ppw/Controller/ordersController.java
+++ ShoppingCart/RestAPI/ppw/src/main/java/com/example/ppw/Controller/ordersController.java
@@ -40,6 +40,16 @@ class CetakAngka{
     }
 }
 
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
 
 public class ContohIfElse {
 
