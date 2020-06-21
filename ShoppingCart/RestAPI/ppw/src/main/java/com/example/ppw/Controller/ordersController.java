3a9a4f68e3ec2f81c927e98ad1ba30df88f76a76
Markolim <54051867+Markolim@users.noreply.github.com>
2020-06-21 23:36:06 +0700

    Update ordersController.java

--- ShoppingCart/RestAPI/ppw/src/main/java/com/example/ppw/Controller/ordersController.java
+++ ShoppingCart/RestAPI/ppw/src/main/java/com/example/ppw/Controller/ordersController.java
@@ -62,3 +62,30 @@ public class ContohIfElse {
     }
 
 }
+
+//import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+
+        // nextDouble() reads the next double from the keyboard
+        double first = reader.nextDouble();
+        double second = reader.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = reader.next().charAt(0);
+
+        double result;
+
+        switch(operator)
+        {
+            
+        }
+
+        System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result);
+    }
+}
