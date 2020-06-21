3220e12e2ca1eb15d00cc51f03d393c6c6589b69
denijosua <denijoshuasamosir@gmail.com>
2020-06-21 15:02:17 +0700

    commit pertama

--- ordersController.java
+++ ordersController.java
@@ -38,3 +38,55 @@ class CetakAngka{
 
     }
 }
+
+public class ContohIfElse {
+
+    public static void main(String[] args) {
+        int nilai = 10;
+        String pesan="", jumlah="";
+
+        if (nilai>2) {
+            jumlah = "Lebih besar dari 2";
+            if(nilai%2 == 0){
+                pesan = "Bilangan Genap";
+            }else{
+                pesan = "Bilangan Ganjil";
+            }
+            System.out.println(jumlah+" dan "+pesan);
+        }else{
+            jumlah = "Lebih kecil dari 2";
+            System.out.println(jumlah);
+        }
+    }
+
+}
+public class ContohPercabanganSwitch {
+
+   public static void main(String args[]) {
+
+      char grade = 'A';
+        
+      switch(grade) {
+         case 'A' :
+            System.out.println("Perfect!");
+            // Statements
+            break;
+            // You can have any number of case statements.
+         case 'B' :
+            System.out.println("Excellent!");
+            // Statements
+            break;
+         case 'C' :
+            System.out.println("Well done");
+            break;
+         case 'D' :
+            System.out.println("You passed");
+         case 'F' :
+            System.out.println("Better try again");
+            break;
+         default :
+            System.out.println("Invalid grade");
+      }
+      System.out.println("Your grade is " + grade);
+   }
+}
