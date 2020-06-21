61a4571e7cfd5772d50b13a2b21b3c64d2356580
Octaviany <63240238+octav17@users.noreply.github.com>
2020-06-22 01:11:55 +0700

    Update Sorting.java

--- src/main/java/shop/Sorting.java
+++ src/main/java/shop/Sorting.java
@@ -45,3 +45,12 @@ public class Sorting {
         public void setWeight(Integer weight) {
             this.weight = weight;
         }
+
+            
+            
+    static class AppleComparator implements Comparator<Apple> {
+        public int compare(Apple a1, Apple a2){
+            return a1.getWeight().compareTo(a2.getWeight());
+        }
+    }
+}
