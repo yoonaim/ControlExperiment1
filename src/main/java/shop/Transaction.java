f851edd1d69ca4deebe136cde4b2fc4962b2cc67
Octaviany <63240238+octav17@users.noreply.github.com>
2020-06-22 01:23:05 +0700

    Update Transaction.java

--- src/main/java/shop/Transaction.java
+++ src/main/java/shop/Transaction.java
@@ -12,5 +12,22 @@ public class Transaction{
 		this.year = year;
 		this.value = value;
 	}
+	public Trader getTrader(){ 
+		return this.trader;
+	}
+
+	public int getYear(){
+		return this.year;
+	}
+
+	public int getValue(){
+		return this.value;
+	}
+	
+	public String toString(){
+	    return "{" + this.trader + ", " +
+	           "year: "+this.year+", " +
+	           "value:" + this.value +"}";
+	}
 
 }
