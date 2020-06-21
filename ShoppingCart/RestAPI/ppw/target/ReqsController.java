77034a1a055b9ca52b09167181bcdf807a03a09d
Fitri <38976894+FitrianiGitana3@users.noreply.github.com>
2020-06-21 19:29:08 +0700

    Fungsi Request
    Ini untuk Fungsi Request

--- ShoppingCart/RestAPI/ppw/target/ReqsController.java
+++ ShoppingCart/RestAPI/ppw/target/ReqsController.java
@@ -1,3 +1,4 @@
+//fungsi untuk menambah belanja
 package com.example.ppw.controller;
 
 import com.example.ppw.model.orders;
@@ -5,6 +6,7 @@ import com.example.ppw.repository.ordersRepo;
 import com.example.ppw.repository.testNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 import java.util.Optional;
