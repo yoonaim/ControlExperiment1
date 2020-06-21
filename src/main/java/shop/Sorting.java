15235f33aa7f60d099b30141ebec5d279d4dfa92
Octaviany <63240238+octav17@users.noreply.github.com>
2020-06-22 01:09:09 +0700

    Update Sorting.java

--- src/main/java/shop/Sorting.java
+++ src/main/java/shop/Sorting.java
@@ -14,3 +14,12 @@ public class Sorting {
         // [Apple{color='green', weight=80}, Apple{color='red', weight=120}, Apple{color='green', weight=155}]
         inventory.sort(new AppleComparator());
         System.out.println(inventory);
+
+        // reshuffling things a little
+        inventory.set(1, new Apple(30, "green"));
+        
+        // 2
+        // [Apple{color='green', weight=30}, Apple{color='green', weight=80}, Apple{color='green', weight=155}]
+        inventory.sort(new Comparator<Apple>() {
+            public int compare(Apple a1, Apple a2){
+                return a1.getWeight().compareTo(a2.getWeight()); 
